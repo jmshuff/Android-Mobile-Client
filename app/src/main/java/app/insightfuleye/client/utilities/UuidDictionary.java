25c72d737f5b95b9a69011d7a4ecf6ce0e12920d
jmshuff <37374473+jmshuff@users.noreply.github.com>
2021-09-02 15:15:14 -0400

    Add Physical Exam encounter and concepts for VA and Pinhole

--- app/src/main/java/app/insightfuleye/client/utilities/UuidDictionary.java
+++ app/src/main/java/app/insightfuleye/client/utilities/UuidDictionary.java
@@ -23,7 +23,14 @@ public class UuidDictionary {
     public static final String FOLLOW_UP_VISIT = "e8caffd6-5d22-41c4-8d6a-bc31a44d0c86";
     //m added
     public static final String EMERGENCY = "ca5f5dc3-4f0b-4097-9cae-5cf2eb44a09c";
-//physical exam
+//physical exam JS
+    public static final String VARight = "4765816f-dffe-42d4-bef2-7eaba8e191b6";
+    public static final String VALeft = "bc4744da-8e42-48f2-a061-de0ea13c02ad";
+    public static final String PinholeRight = "50492e1a-db2c-438f-bd9b-f8b7c68920f8";
+    public static final String PinholeLeft = "e7f1a349-8bb3-44a2-9d6a-260fffd01e7b";
+    public static final String VolunteerReferral = "f83b9e96-8247-4354-9ba6-1196e3b56500";
+    public static final String VolunteerReferralReason = "d33cecbf-03b8-47ca-8ef7-771343c31d40";
+    public static final String VolunteerReferralLocation= "413a81ab-35a7-4660-8a66-9f29dd33e2bf";
 
     public static final String TELEMEDICINE_DIAGNOSIS = "537bb20d-d09d-4f88-930b-cc45c7d662df";
     public static final String JSV_MEDICATIONS = "c38c0c50-2fd2-4ae3-b7ba-7dd25adca4ca";
@@ -44,7 +51,7 @@ public class UuidDictionary {
     public static final String ENCOUNTER_VISIT_NOTE = "d7151f82-c1f3-4152-a605-2f9ea7414a79";
     public static final String ENCOUNTER_VISIT_COMPLETE = "bd1fbfaa-f5fb-4ebd-b75c-564506fc309e";
     public static final String ENCOUNTER_PATIENT_EXIT_SURVEY = "629a9d0b-48eb-405e-953d-a5964c88dc30";
-    public static final String ENCOUNTER_PHYSICAL_EXAM = "";
+    public static final String ENCOUNTER_PHYSICAL_EXAM = " e80190f4-374f-4d1b-b52d-b315328618b7 ";
 
     public static final String EMERGENCY_OBS = "ca5f5dc3-4f0b-4097-9cae-5cf2eb44a09c";
 
