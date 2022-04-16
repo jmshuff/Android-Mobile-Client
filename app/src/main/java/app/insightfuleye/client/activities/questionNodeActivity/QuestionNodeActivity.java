cafc1f97d9b7e4c750534935be396f3c10456df1
jmshuff <37374473+jmshuff@users.noreply.github.com>
2022-04-16 10:21:12 -0400

    update single selection

--- app/src/main/java/app/insightfuleye/client/activities/questionNodeActivity/QuestionNodeActivity.java
+++ app/src/main/java/app/insightfuleye/client/activities/questionNodeActivity/QuestionNodeActivity.java
@@ -284,23 +284,28 @@ public class QuestionNodeActivity extends AppCompatActivity implements Questions
             }
         } else if ((currentNode.getOption(groupPosition).getChoiceType().equals("single")) && currentNode.getOption(groupPosition).anySubSelected()) {
             Node question = currentNode.getOption(groupPosition).getOption(childPosition);
-            if(question.isSelected()){
-                question.toggleSelected();
-                currentNode.getOption(groupPosition).setUnselected();
+            if (currentNode.getOption(groupPosition).isBilateral()){
+
             }
             else{
-                MaterialAlertDialogBuilder alertDialogBuilder = new MaterialAlertDialogBuilder(this);
-                //AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(QuestionNodeActivity.this,R.style.AlertDialogStyle);
-                alertDialogBuilder.setMessage(R.string.this_question_only_one_answer);
-                alertDialogBuilder.setNeutralButton(R.string.generic_ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
-                IntelehealthApplication.setAlertDialogCustomTheme(this, alertDialog);
+                if(question.isSelected()){
+                    question.toggleSelected();
+                    currentNode.getOption(groupPosition).setUnselected();
+                }
+                else {
+                    MaterialAlertDialogBuilder alertDialogBuilder = new MaterialAlertDialogBuilder(this);
+                    //AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(QuestionNodeActivity.this,R.style.AlertDialogStyle);
+                    alertDialogBuilder.setMessage(R.string.this_question_only_one_answer);
+                    alertDialogBuilder.setNeutralButton(R.string.generic_ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+                    IntelehealthApplication.setAlertDialogCustomTheme(this, alertDialog);
+                }
             }
 
         } else {
